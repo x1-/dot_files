6678a39aa5cb53aa2c0c36bf29e69c739369c2e2
x1- <viva008@gmail.com>
2017-08-11 23:34:16 +0900

    modified for tmux 2.5 and fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,12 @@ if test -e ~/.config/fish/alias.fish
   . ~/.config/fish/alias.fish
 end
 
+### specific
+if test -e ~/.config/fish/specific.fish
+  . ~/.config/fish/specific.fish
+end
+
 ### peco
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
-
